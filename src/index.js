86f011aebdef97975f986c844b110c3e610d569c
saduchandravamsi <saduchandravamsi@gmail.com>
2025-11-26 21:43:21 +0530

    connect to mongo altas

---
+++ src/index.js
@@ -0,0 +1,48 @@
+//required('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+    (async () => {
+        try {
+            mongoose.connect(`${process.env.
+                MONGODB_URI}/${DB_NAME}`)
+            app.on("error", () => {
+                console.log("ERROR: ", error)
+                throw error
+            })
+
+
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listening on port ${process.env.PORT}`);
+            })
+        } catch (error) {
+            console.error("ERROR: ", error)
+            throw err
+        }
+    })()
+        */
