60b5184519f1bd39523c8599803022e83e57f11f
saduchandravamsi <saduchandravamsi@gmail.com>
2025-11-27 21:13:59 +0530

    Access Tokens and Models defined

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, // cloudnary response
+            required: true
+        },
+        views: {
+            type: Number,
+            ddefault: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+
+        }
+    },
+    {
+        timestamps: true
+    })
+
+videoSchema.plubin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video", videoSchema)
+
