9d86f2f2f6c59902935a1f36e82f5da1c82ef57c
saduchandravamsi <saduchandravamsi@gmail.com>
2025-12-01 16:53:27 +0530

    Initiate with postman

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 //required('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
-
+import express from "express"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
     path: './env'
 })
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000), () => {
-            console.log(` Server is running at port : ${process.env.PORT}`);
-        }
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT || 8000}`);
+        });
+
     })
     .catch((err) => {
         console.log("MONGO DB CONNECTION FAILED !!!", err);
